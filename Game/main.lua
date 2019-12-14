133046d612f474eb603e8e9dc6e739765c1eaba0
Jacob <jacobjdowning@gmail.com>
2019-12-13 20:28:51 -0500

    set up test grid and changed window size

--- Game/main.lua
+++ Game/main.lua
@@ -3,12 +3,23 @@ local Sprite = require "sprites.Sprite"
 local anims = require "assets.anims"
 
 function love.load()
-	hex = love.graphics.newImage('assets/41.png', format)
-	love.graphics.setBackgroundColor(1, 1, 1, 1)
+	hex = love.graphics.newImage('assets/hex.png', format)
+	love.graphics.setBackgroundColor(0, 1, 1, 1)
+	local success = love.window.setMode(1920, 1020)
+	hexWidth, hexHeight = hex:getDimensions() 
 end
 
 function love.draw()
-	love.graphics.draw(hex, 100, 100)
+	for l=0,1 do
+		for j=0,5 do
+			for i=0,9 do
+				love.graphics.draw(hex,
+					-- hexWidth*3/4
+					l*(29*3)+100+(i*hexWidth*1.5),
+					l*(hexHeight/2) +200 + j * hexHeight)
+			end
+		end
+	end 
 	-- batch:clear()
 	-- 	-- Draw here
 	-- love.graphics.draw(batch)
